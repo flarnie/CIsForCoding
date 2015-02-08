4fb568b5356074182c78c43bbaf0b2dc5c9d9d9c
Flarnie Marchan <fnonemaker@gmail.com>
2015-02-07 16:12:22 -0800

    Add symbolic constants to fah to cels

--- TheCProgrammingLanguage/Chapter1/exercises/fah_to_cels.c
+++ TheCProgrammingLanguage/Chapter1/exercises/fah_to_cels.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
+
 /* print Fahrenheit-Celsius table
  * for fahr = 0, 20, ..., 300 */
-
 int main()
 {
   printf("Fahr.  |  Cels\n");
   float fahr, celsius;
 
-  for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
     celsius = (5.0/9.0)  * (fahr - 32.0);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
   }
