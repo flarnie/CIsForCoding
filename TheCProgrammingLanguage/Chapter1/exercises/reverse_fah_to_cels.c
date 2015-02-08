d90d230f809db3b9c42de85cbd3591bf66af42fe
Flarnie Marchan <fnonemaker@gmail.com>
2015-02-07 16:16:55 -0800

    Add reverse printing of fah to cels

---
+++ TheCProgrammingLanguage/Chapter1/exercises/reverse_fah_to_cels.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define LOWER 0 /* lower limit of the table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
+
+/* print Fahrenheit-Celsius table */
+int main()
+{
+  int fahr;
+
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+  }
+
+  return 0;
+}
