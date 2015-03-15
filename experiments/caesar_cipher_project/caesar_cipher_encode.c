4078adbf6173fd0b3ca4ffb344b5d110e7f136ff
Flarnie Marchan <fnonemaker@gmail.com>
2015-03-15 10:22:26 -0700

    Allow Caesar Cipher to handle upper case
    Also deleted the compiled version of the program, to keep things tidy.

--- experiments/caesar_cipher_project/caesar_cipher_encode.c
+++ experiments/caesar_cipher_project/caesar_cipher_encode.c
@@ -5,11 +5,18 @@
 #define CIPHER_SHIFT 3 /* how many places to shift the letter */
 
 char alphabet_lower[(ALPHABET_LENGTH + 1)] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
+char alphabet_upper[(ALPHABET_LENGTH + 1)] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '\0'};
 
 int get_index(char s) {
   for(int i = 0; i <= ALPHABET_LENGTH; i++) {
-    if (alphabet_lower[i] == s)
-      return i;
+    if (isupper(s)) {
+      if (alphabet_upper[i] == s)
+        return i;
+    }
+    else {
+      if (alphabet_lower[i] == s)
+        return i;
+    }
   }
   return -1;
 };
@@ -21,7 +28,10 @@ int get_encoded_index(int original_index, int modifier) {
 char encode_character(char s, int modifier) {
   int index = get_index(s);
   int encodedIndex = get_encoded_index(index, modifier);
-  return alphabet_lower[encodedIndex];
+  if (isupper(s))
+    return alphabet_upper[encodedIndex];
+  else
+    return alphabet_lower[encodedIndex];
 }
 
 int main() {
