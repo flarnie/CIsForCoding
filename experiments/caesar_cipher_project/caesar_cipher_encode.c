c457e66af4be06f07d12de14ad043cb4828ca8c9
Flarnie Marchan <fnonemaker@gmail.com>
2015-03-15 10:19:06 -0700

    Initial version of caesar cipher

---
+++ experiments/caesar_cipher_project/caesar_cipher_encode.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define ALPHABET_LENGTH 26 /* number of letters in alphabet */
+#define CIPHER_SHIFT 3 /* how many places to shift the letter */
+
+char alphabet_lower[(ALPHABET_LENGTH + 1)] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
+
+int get_index(char s) {
+  for(int i = 0; i <= ALPHABET_LENGTH; i++) {
+    if (alphabet_lower[i] == s)
+      return i;
+  }
+  return -1;
+};
+
+int get_encoded_index(int original_index, int modifier) {
+  return (original_index + modifier) % ALPHABET_LENGTH;
+}
+
+char encode_character(char s, int modifier) {
+  int index = get_index(s);
+  int encodedIndex = get_encoded_index(index, modifier);
+  return alphabet_lower[encodedIndex];
+}
+
+int main() {
+
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (isalpha(c))
+      printf("%c", encode_character(c, CIPHER_SHIFT));
+    else
+      printf("%c", c);
+  }
+
+  return 0;
+}
+
