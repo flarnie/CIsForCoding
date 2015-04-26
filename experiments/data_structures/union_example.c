8677527f119a9d136d3c2eb7c8d75d172a6eac20
Flarnie Marchan <fnonemaker@gmail.com>
2015-04-26 11:17:37 -0700

    Add union example

--- experiments/data_structures/union_example.c
+++ experiments/data_structures/union_example.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum { COUNT, WEIGHT, VOLUME } quantity_types;
+
 // Create a struct with a nested union
 typedef struct {
   const char* type;
@@ -9,15 +11,15 @@ typedef struct {
     float weight;
     float volume;
   } quantity;
-  char quantity_type; // 'c' for count, 'w' for weight, 'v' for volume
+  quantity_types quantity_type;
 } fruit_order;
 
 void print_fruit_order(fruit_order this_order) {
-  if (this_order.quantity_type == 'c') {
+  if (this_order.quantity_type == COUNT) {
     printf("Fruit Order:\n  Type: %s\n  Quantity: %i\n", this_order.type, this_order.quantity.count);
-  } else if (this_order.quantity_type == 'w') {
+  } else if (this_order.quantity_type == WEIGHT) {
     printf("Fruit Order:\n  Type: %s\n  Quantity: %.2f\n", this_order.type, this_order.quantity.weight);
-  } else if (this_order.quantity_type == 'v') {
+  } else if (this_order.quantity_type == VOLUME) {
     printf("Fruit Order:\n  Type: %s\n  Quantity: %.2f\n", this_order.type, this_order.quantity.volume);
   }
 };
@@ -25,9 +27,9 @@ void print_fruit_order(fruit_order this_order) {
 // fruit order program
 int main() {
   // create three fruit orders
-  fruit_order apples = {"apples", 6, .quantity_type='c'};
-  fruit_order strawberries = {"strawberries", .quantity.weight=1.5, .quantity_type='w'};
-  fruit_order orange_juice = {"orange juice", .quantity.volume=0.5, .quantity_type='v'};
+  fruit_order apples = {"apples", 6, .quantity_type = COUNT};
+  fruit_order strawberries = {"strawberries", .quantity.weight=1.5, .quantity_type = WEIGHT};
+  fruit_order orange_juice = {"orange juice", .quantity.volume=0.5, .quantity_type = VOLUME};
 
   // print out the fruit orders
   print_fruit_order(apples);
